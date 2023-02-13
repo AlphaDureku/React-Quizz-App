433a3e1bdccc6e7538036e0b42c34f1a69355fec
unknown <templanzamark2002@gmail.com>
2023-02-13 21:29:06 +0800

    fixed

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,90 +1,131 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import IndividualQuestions from "./IndividualQuestions";
+import Confetti from "react-confetti";
 
-import IndividualQuestions from './IndividualQuestions';
-import Confetti from 'react-confetti'
+export default function Questions(props) {
+  const [quiz, setQuiz] = useState([]);
+  const [score, setScore] = useState({ score: 0, status: false });
+  const [restart, setRestart] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-export default function Questions(props){
-    const [quiz, setQuiz] =useState([])
-    const [score, setScore]=useState({score: 0, status: false})
-    const [restart, setRestart]=useState(false)
+  useEffect(() => {
+    setLoading(true);
+    async function fetchAPI() {
+      setScore({ score: 0, status: false });
+      const data = await fetch(
+        "https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple"
+      );
+      let res = await data.json();
+      setQuiz(res.results);
+      concatenateChoices();
+    }
+    fetchAPI();
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [restart]);
 
-        useEffect(()=>{
-        async function fetchAPI(){
-            setScore({score: 0, status: false})
-            const data = await fetch("https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple")
-            let res = await data.json()
-            setQuiz(res.results)
-            concatenateChoices()
-        }   
-        fetchAPI()
-    }, [restart])
-    
-    function insertRandom(array, item) {
-        const randomIndex = Math.floor(Math.random() * (array.length + 1));
-        array.splice(randomIndex, 0, item);
-        return array;
+  function insertRandom(array, item) {
+    const randomIndex = Math.floor(Math.random() * (array.length + 1));
+    array.splice(randomIndex, 0, item);
+    return array;
+  }
+
+  function concatenateChoices() {
+    setQuiz((prevData) =>
+      prevData.map((item, index) => {
+        return {
+          ...item,
+          choices: insertRandom(item.incorrect_answers, item.correct_answer),
+          id: index,
+        };
+      })
+    );
+    setQuiz((prevData) =>
+      prevData.map((item, index) => {
+        return { ...item, question: item.question.replace("&quot;", "") };
+      })
+    );
+    setQuiz((prevData) =>
+      prevData.map((item, index) => {
+        return { ...item, question: item.question.replace("&#039;s", "") };
+      })
+    );
+    setQuiz((prevData) =>
+      prevData.map((item, index) => {
+        return { ...item, question: item.question.replace("&quot;", "") };
+      })
+    );
+  }
+
+  function setAnswer(answered, id) {
+    setQuiz((prevData) =>
+      prevData.map((item) => {
+        return item.id === id ? { ...item, answer: answered } : item;
+      })
+    );
+  }
+
+  function checkAnswer() {
+    setScore((prev) => ({ ...prev, status: true }));
+    for (let i = 0; i < quiz.length; i++) {
+      if (quiz[i].answer === quiz[i].correct_answer) {
+        setScore((prevData) => ({
+          ...prevData,
+          score: prevData.score + 1,
+        }));
       }
-
-    function concatenateChoices(){
-        setQuiz(prevData=>(
-            prevData.map((item, index)=>{
-                return {...item, choices: insertRandom(item.incorrect_answers, item.correct_answer), id: index}
-            }))
-        )
-        setQuiz(prevData=>(
-            prevData.map((item, index)=>{
-                return {...item, question: item.question.replace("&quot;", "")}
-            }))
-        )
-        setQuiz(prevData=>(
-            prevData.map((item, index)=>{
-                return {...item, question: item.question.replace("&#039;s", "")}
-            }))
-        )
-        setQuiz(prevData=>(
-            prevData.map((item, index)=>{
-                return {...item, question: item.question.replace("&quot;", "")}
-            }))
-        )
     }
+  }
 
-    function setAnswer(answered, id){
-        setQuiz(prevData=>(
-            prevData.map((item)=>{   
-                return item.id === id ? {...item, answer: answered} : item
-            }))
-        )
-    }
+  function handleRestart() {
+    setRestart(!restart);
+  }
+  let quizElements = quiz.map((items, index) => {
+    return (
+      <IndividualQuestions
+        key={index}
+        questions={items.question}
+        choices={items.choices}
+        UserAnswer={setAnswer}
+        id={index}
+        answer={items.answer}
+        status={score.status}
+        correct={items.correct_answer}
+      />
+    );
+  });
 
-    function checkAnswer(){
-        setScore(prev=>({...prev, status: true}))
-        for(let i=0; i<quiz.length; i++){
-            if(quiz[i].answer === quiz[i].correct_answer){
-              setScore(prevData=>({
-                ...prevData, score: prevData.score + 1
-              }))
-              
-            }
-        }
-    }
+  if (loading) {
+    return (
+      <div className="loading-wrapper">
+        <img
+          src="https://i.pinimg.com/originals/ae/1b/16/ae1b16df146614b08eca447625394346.gif"
+          alt=""
+        ></img>
+      </div>
+    );
+  }
 
-    function handleRestart(){
-
-        setRestart((!restart))
-    }
-    let quizElements = quiz.map((items, index)=>{
-        return <IndividualQuestions key={index}questions={items.question} choices={items.choices} UserAnswer ={setAnswer} id = {index} answer = {items.answer} status = {score.status} correct = {items.correct_answer}/>
-    })
-
-    return(
-        <div className="question-wrapper">
-             {quizElements}
-             <div className='check_answer_btn-wrapper'>
-             {score.status && <h1 className='score'>You scored {score.score}/{quiz.length}</h1>}
-             <button className='check_answer_btn' onClick={!score.status ? checkAnswer : handleRestart}>{!score.status ? 'Check Answer' : 'Restart'}</button>
-             {score.status && <Confetti height={2500}/>}
-             </div>
-             
-        </div>
-    )
+  return (
+    <div className="question-wrapper">
+      {quizElements || "LoadingsyncAll()"}
+      <div className="check_answer_btn-wrapper">
+        {score.status && (
+          <h1 className="score">
+            You scored {score.score}/{quiz.length}
+          </h1>
+        )}
+        <button
+          className="check_answer_btn"
+          onClick={!score.status ? checkAnswer : handleRestart}
+        >
+          {!score.status ? "Check Answer" : "Restart"}
+        </button>
+        {score.status && <Confetti height={2500} />}
+      </div>
+    </div>
+  );
 }
