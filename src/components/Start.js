1238879ed7dc9d611cbc874bff6638a246b61a1e
AlphaDureku <templanzamark2002@gmail.com>
2023-07-03 20:29:19 +0800

    update

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,12 +1,12 @@
-
-
-export default function Start(props){
-    return(
-        <div className="start-wrapper">
-            <h1 className="title">Quizzical</h1>
-            <p className="description">Some Description if needed</p>
-            <button className="start-btn" onClick={props.start}>Start Quiz</button>
-            <div className="bg-img"></div>
-        </div>
-    )
+export default function Start(props) {
+  return (
+    <div className="start-wrapper">
+      <h1 className="title">Quizzical</h1>
+      <p className="description">Test your knowledge about anime!</p>
+      <button className="start-btn" onClick={props.start}>
+        Start Quiz
+      </button>
+      <div className="bg-img"></div>
+    </div>
+  );
 }
